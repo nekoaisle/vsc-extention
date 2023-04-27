391e1fa91010f739beee3d9def4b9103d603d093
Yoshio Kiya <kiya@creans.jp>
2023-04-27 16:48:17 +0900

    nekoaisle-exec 最新対応 execInsert追加

--- nekoaisle-exec/src/extension.ts
+++ nekoaisle-exec/src/extension.ts
@@ -12,7 +12,7 @@
 'use strict';
 import * as vscode from 'vscode';
 import {Util} from './nekoaisle.lib/Util';
-import {Extension} from './nekoaisle.lib/Extension';
+import { Extension, EditInsert } from './nekoaisle.lib/Extension';
 
 /**
  * エクステンション活性化
@@ -50,6 +50,12 @@ class MyExtention extends Extension {
 					callback: (arg?: string) => {
 						this.exec(arg);
 					}
+				},
+				{
+					command: 'nekoaisle.execInsert',	// コマンド
+					callback: (arg?: string) => {
+						this.execInsert(arg);
+					}
 				}
 			]
 		});
@@ -78,5 +84,44 @@ class MyExtention extends Extension {
 
 		// 実行
 		Util.execCmd(cmd);
+	}
+
+	/**
+	 * 実行して結果を挿入
+	 */
+	public async execInsert(cmd?: string) {
+		const editor = vscode.window.activeTextEditor;
+		if (!editor) {
+			return;
+		}
+
+		if (!cmd) {
+			// コマンドが指定されなかったので選択範囲かカーソル行
+			// 選択範囲はある？
+			if (!editor.selection.isEmpty) {
+				// 選択範囲を取得
+				cmd = editor.document.getText(editor.selection);
+			} else {
+				// 選択範囲がないのでカーソル行を取得
+				cmd = Util.getCursorLine(editor);
+			}
+		}
+
+		// 実行
+		let res = Util.execCmd(cmd);
+
+		// カーソルを行末に移動
+		await vscode.commands.executeCommand('cursorEnd');
+
+		// 結果を挿入
+		res = "\n" + res;
+		let inserts: EditInsert[] = [];
+		for (let sels of editor.selections) {
+			inserts.push({
+				pos: sels.anchor,
+				str: res,
+			});
+		}
+		this.syncInsert(editor, inserts);
 	}
 }
