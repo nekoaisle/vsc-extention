79ecbff10d87423306035dc7ae6c561d81cd76ee
Yoshio Kiya <kiya@creans.jp>
2023-08-01 13:15:43 +0900

    disp-char-code 特殊文字の略号を表示するようにした

--- nekoaisle-disp-char-code/README.md
+++ nekoaisle-disp-char-code/README.md
@@ -4,7 +4,8 @@
 ステータスバーにカーソル位置の文字コードを表示します。
 
 ## 設定
-settings.json にて表示フォーマットを変更できます。
+### nekoaisle-disp-char-code.format
+表示フォーマットを指定します。
 
 "nekoaisle-disp-char-code.format": "%c %h (%d)"
 
@@ -14,3 +15,11 @@ settings.json にて表示フォーマットを変更できます。
 | %d   | カーソル位置の文字コード10進表記         |
 | %h   | カーソル位置の文字コード16進表記(小文字) |
 | %H   | カーソル位置の文字コード16進表記(大文字) |
+
+※ %c: 制御文字は略号を通常文字は '' でくくって表示します。
+
+### nekoaisle-disp-char-code.charBefore, charAfter
+通常文字をくくる文字列を指定します。
+
+"nekoaisle-disp-char-code.charBefore": "'"
+"nekoaisle-disp-char-code.charAfter": "'"
--- nekoaisle-disp-char-code/src/extension.ts
+++ nekoaisle-disp-char-code/src/extension.ts
@@ -23,6 +23,48 @@ class MyExtention extends Extension {
 	private disposable: vscode.Disposable;
 	private statusBarItem: vscode.StatusBarItem;
 	private format: string;
+	private charBefore: string;
+	private charAfter: string;
+
+	/**
+	 * 制御コード表
+	 */
+	private static ctrlCode = [
+		'NULL',	// 00
+		'SOH',	// 01
+		'STX',	// 02
+		'ETX',	// 03
+		'EOT',	// 04
+		'ENG',	// 05
+		'ACK',	// 06
+		'BEL',	// 07
+		'BS',		// 08
+		'HT',		// 09
+		'LF',		// 0A
+		'VT',		// 0B
+		'FF',		// 0C
+		'CR',		// 0D
+		'SO',		// 0E
+		'SI',		// 0F
+		'DLE',	// 10
+		'DC1',	// 11
+		'DC2',	// 12
+		'DC3',	// 13
+		'DC4',	// 14
+		'NAK',	// 15
+		'SYN',	// 16
+		'ETB',	// 17
+		'CAN',	// 18
+		'EM',		// 19
+		'SUB',	// 1A
+		'ESC',	// 1B
+		'FS',		// 1C
+		'GS',		// 1D
+		'RS',		// 1E
+		'US',		// 1F
+		'SPC',	// 20
+	];
+
 	/**
 	 * 構築
 	 */
@@ -44,8 +86,10 @@ class MyExtention extends Extension {
 		this.disposable = vscode.Disposable.from(...subscriptions);
 
 		// 表示フォーマット取得
-		this.format = this.getConfig('format', "'%c' %HH (%d)");
-
+		this.format     = this.getConfig('format', "%c %HH (%d)");
+		this.charBefore = this.getConfig('charBefore', "'");
+		this.charAfter  = this.getConfig('charAfter' , "'");
+	
 		// 初期表示
 		this.dispCharCode();
 	}
@@ -77,11 +121,19 @@ class MyExtention extends Extension {
 		// カーソル位置の文字コードを取得
 		let str: string;
 		if ( line.length > cursor.character ) {
-			const c = line.charAt(cursor.character);
 			const d = line.charCodeAt(cursor.character);
 			const h = d.toString(16);
 			const H = h.toLocaleUpperCase();
-
+			let c;
+			if (d <= 0x20) {
+				c = MyExtention.ctrlCode[d];
+			} else if (d === 0x7f) {
+				c = 'DEL';
+			} else {
+				c = String.fromCharCode(d);
+				c = `${this.charBefore}${c}${this.charAfter}`;
+			}
+	
 			str = this.format;
 			str = str.replace('%c', c);
 			str = str.replace('%d', '' + d);
